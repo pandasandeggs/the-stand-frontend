08d05c303e490ebafb36932d734d9f3dc8722ef2
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-10-02 09:26:31 -0500

    Updated SearchBar, App.js, Filter.js

--- src/App.js
+++ src/App.js
@@ -19,18 +19,21 @@ class App extends Component {
 
   componentDidMount() {
 
-    fetch('https://newsapi.org/v2/everything?sources=associated-press&apiKey=0ee37ea5b6c443cda2481bd0cdb71c13')
+    //fetch('https://newsapi.org/v2/everything?sources=associated-press&apiKey=0ee37ea5b6c443cda2481bd0cdb71c13')
+    fetch('https://newsapi.org/v2/everything?sources=associated-press,medical-news-today,ars-technica,fox-sports,buzzfeed,hacker-news,national-geographic&apiKey=0ee37ea5b6c443cda2481bd0cdb71c13')
       .then(resp => resp.json())
-      .then(articles => this.setState({articles, isLoading:false})
+      .then(data => this.setState({articles: data.articles, isLoading:false})
     )
 
     fetch('https://newsapi.org/v2/sources?apiKey=0ee37ea5b6c443cda2481bd0cdb71c13')
       .then(resp => resp.json())
-      .then(sources => this.setState({sources}))
+      .then(data => this.setState({sources: data.sources}))
 
   }
 
   render() {
+    // console.log('Articles: ', this.state.articles)
+    // console.log('Sources: ', this.state.sources)
     return (
       <div className="App">
         { this.state.isLoading ? <h1>Loading Articles...</h1> :
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,9 +2,45 @@ import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
 
+  state = {
+    searchItem: "All",
+    searchResults:[]
+  }
+
+  handleOnChange = (event) =>{
+    //console.log('handleOnChange', event.target.name, event.target.value)
+    this.setState({[event.target.name]: event.target.value})
+    //Filter Newsfeed
+  }
+
+  handleOnSubmit = (event) =>{
+    let newResults = []
+    event.preventDefault()
+    //console.log('handleOnSubmit');
+    this.setState({searchItem: event.target.value})
+    newResults = this.getSearchResults(this.state.searchItem)
+    this.setState({searchResults: newResults})
+    console.log('newResults',newResults)
+  }
+
+  getSearchResults = (searchItem) =>{
+    console.log('searchItem = ', searchItem)
+    return this.props.articles.filter((article) =>{
+       if (article.content){
+       return article.content.includes(searchItem)}
+    })
+
+  }
+
   render() {
+    // console.log('In SearchBar')
+    // console.log(this.props.articles)
     return(
       <div>
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text" name="searchItem" value={this.searchItem} onChange={this.handleOnChange}/>
+          <input type="submit" value="Search" />
+        </form>
       </div>
     )
   }
